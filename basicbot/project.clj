901b601289161f32ca8032e212216c6db00553dd
Jeff Flower <flower_jeff@yahoo.com>
2016-04-22 22:53:43 -0600

    Combine files into core.clj
    aigames won't compile a project that's in more than one file.  combined
    all the functions into one file.

--- basicbot/project.clj
+++ basicbot/project.clj
@@ -5,7 +5,8 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]]
   :plugins [[lein-marginalia "0.9.0"]]
-  :main ^:skip-aot basicbot.core
+  :main basicbot.core
+  :aot :all
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}
              :dev {:dependencies [[midje "1.6.3"]]}})
