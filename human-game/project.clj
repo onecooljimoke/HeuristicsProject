5f607e04bd398b67d450fb1b3f1114c3d71dfe4c
Jeff <jeff.flower14@gmail.com>
2016-05-13 22:24:33 -0600

    generate docs

--- human-game/project.clj
+++ human-game/project.clj
@@ -4,7 +4,9 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.clojure/core.async "0.2.374"]]
+                 [org.clojure/core.async "0.2.374"]
+                 [org.toomuchcode/clara-rules "0.11.0"]]
+  :plugins [[lein-marginalia "0.9.0"]]
   :main ^:skip-aot human-game.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
